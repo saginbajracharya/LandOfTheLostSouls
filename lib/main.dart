e72f90090d2bd43ee3ff9ccdc03f12bd7f2f5b62
sagin.bajracharya <sagin@miracleinterface.com>
2022-07-28 10:47:59 +0545

    [test]

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
-void main() async {
+void main() async { //
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
     [
